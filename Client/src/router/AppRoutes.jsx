5a42cfcf29c014cd9c7a4210d180191639324367
MOHITH2511 <mohithragul25@gmail.com>
2024-12-05 13:52:29 +0530

    [Update] AppRoutes

--- Client/src/router/AppRoutes.jsx
+++ Client/src/router/AppRoutes.jsx
@@ -5,11 +5,21 @@ import HomePage from "../Pages/HomePage";
 import FindTutors from "../Pages/FindTutors";
 import SubscriptionPlans from "../components/homepage/SubscriptionPlans";
 import PaymentPage from "../Pages/SubscribePage";
+import StudentDashboard from "../pages/";
+import TutorDashboard from "../pages/TutorDashboard.jsx";
+import AssignmentStatus from "../pages/AssignmentStatus.jsx";
+import AssignmentPendingPage from "../pages/AssignmentPending.jsx";
+import Signup from "../components/Signup/Signup.jsx";
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/student-dashboard" element={<StudentDashboard />} />
+      <Route path="/tutor-dashboard" element={<TutorDashboard />} />
+      <Route path="/assignment-status" element={<AssignmentStatus />} />
+      <Route path="/pending-assignments" element={<AssignmentPendingPage />} />
       <Route path="/find-tutors" element={<FindTutors />} />
       <Route path="/subscription" element={<SubscriptionPlans />} />
       <Route path="/payment" element={<PaymentPage />} />
